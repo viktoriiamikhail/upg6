d8ff61479fb5a30bb25f53bc402df18c2d088bcd
viktoriiamikhail <viktoriia.mikhailova@axxell.fi>
2022-11-04 13:54:16 +0200

    Skapade index.html

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=2022_tidsbokning;charset=utf8mb4", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
